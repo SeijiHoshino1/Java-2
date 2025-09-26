c16ebd6c526476db62bfdba09fddadc3708a04b3
SeijiHoshino1 <seijihoshino@outlook.com>
2025-09-26 15:16:37 -0300

    feat(constructors): add study about constructors

--- src/src/main/java/com/studies/constructor/domain/Anime.java
+++ src/src/main/java/com/studies/constructor/domain/Anime.java
@@ -1,4 +1,33 @@
 package com.studies.constructor.domain;
 
 public class Anime {
+    private String nome;
+    private String tipo;
+    private int episodios;
+    private String genero;
+    private String estudio;
+
+    public Anime() {}
+
+    public Anime(String nome, String tipo, int episodios, String genero) {
+        this();
+        this.nome = nome;
+        this.tipo = tipo;
+        this.episodios = episodios;
+        this.genero = genero;
+    }
+
+    // Chamando um construtor dentro de outro construtor
+    public Anime(String nome, String tipo, int episodios, String genero, String estudio) {
+        this(nome, tipo, episodios, genero);
+        this.estudio = estudio;
+    }
+
+    public void imprimir() {
+        System.out.println(this.nome);
+        System.out.println(this.tipo);
+        System.out.println(this.episodios);
+        System.out.println(this.genero);
+        System.out.println(this.estudio);
+    }
 }
--- src/src/main/java/com/studies/constructor/test/AnimeTest01.java
+++ src/src/main/java/com/studies/constructor/test/AnimeTest01.java
@@ -1,4 +1,11 @@
 package com.studies.constructor.test;
 
+import com.studies.constructor.domain.Anime;
+
 public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Haikyuu", "TV", 5, "Ação", "Studio IG");
+
+        anime.imprimir();
+    }
 }
