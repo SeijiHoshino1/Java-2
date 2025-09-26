09e60f944ecfc611745a7e07c5bb0171800b8158
SeijiHoshino1 <seijihoshino@outlook.com>
2025-09-26 15:17:26 -0300

    feat(initblocks): add study about initialization blocks

--- src/src/main/java/com/studies/initblocks/test/AnimeTest.java
+++ src/src/main/java/com/studies/initblocks/test/AnimeTest.java
@@ -1,4 +1,12 @@
 package com.studies.initblocks.test;
 
+import com.studies.initblocks.domain.Anime;
+
 public class AnimeTest {
+    public static void main(String[] args) {
+        Anime anime = new Anime();
+        for (int episodio : anime.getEpisodios()) {
+            System.out.println("EP: " + episodio);
+        }
+    }
 }
